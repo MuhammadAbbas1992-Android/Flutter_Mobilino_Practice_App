8efaec05a062950637f3438ea2be2f92a3d567f1
Muhammad Abbas <g.abbas1992developer@gmail.com>
2024-11-21 18:00:43 +0500

    Started Project

---
+++ lib/views/splash/splash_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: Center(child: Text('Welcome To Mobilino App'))),
+    );
+  }
+}
